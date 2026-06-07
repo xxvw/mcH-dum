0d7acdfb9f88014869d305df9e783019646c374c
xxvw <tokuhara@gamers-buddy.com>
2026-06-07 23:18:48 +0900

    chore: prepare Spring data web dependencies

--- src/test/java/com/github/n9/mch/McHostApplicationTests.java
+++ src/test/java/com/github/n9/mch/McHostApplicationTests.java
@@ -3,7 +3,7 @@ package com.github.n9.mch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "mch.minecraft.auto-update-paper=false")
 class McHostApplicationTests {
 
 	@Test
